4b3f698fd94b418c5fdf379791523e1666adefd1
James Chuang <chuang.ja@gmail.com>
2018-08-17 11:06:42 -0400

    Add updated Spt6 western blot image to figure 1B.

--- scripts/spt6_2018_figure1B.R
+++ scripts/spt6_2018_figure1B.R
@@ -1,39 +1,38 @@
 
-main = function(theme_spec, data_path,
-                spt6_blot_path, dst1_blot_path,
+main = function(theme_spec, data_path, blot_path,
                 fig_width, fig_height,
                 svg_out, pdf_out, png_out, grob_out){
     source(theme_spec)
     library(png)
 
     df = read_tsv(data_path) %>%
-        select(1,2,3,4,5,18,19,20,21,22) %>%
+        select(1,2,3,4,5,18,19,20,21,22) %>% 
         magrittr::set_colnames(c("strain", "temperature", "antigen", "background", "replicate",
-                                 "gray_min", "grey_max", "gray_mean", "gray_median", "auc")) %>%
-        select(strain, temperature, antigen, background, replicate, auc) %>%
-        spread(background, auc) %>%
-        mutate(signal = pmax(`FALSE`-`TRUE`, 0)) %>%
-        select(-c(`FALSE`, `TRUE`)) %>%
-        spread(antigen, signal) %>%
-        mutate(spikenorm = Spt6/spikein) %>%
-        group_by(strain, temperature) %>%
+                                 "gray_min", "grey_max", "gray_mean", "gray_median", "auc")) %>% 
+        select(strain, temperature, antigen, background, replicate, auc) %>% 
+        spread(background, auc) %>% 
+        mutate(signal = pmax(`FALSE`-`TRUE`, 0)) %>% 
+        select(-c(`FALSE`, `TRUE`)) %>% 
+        spread(antigen, signal) %>% 
+        mutate(spikenorm = Spt6/spikein) %>% 
+        group_by(strain, temperature) %>% 
         mutate(group_mean = mean(spikenorm, na.rm=TRUE)) %>%
-        ungroup() %>%
+        ungroup() %>% 
         mutate(strain = ordered(strain, levels = c("WT", "spt6-1004")))
-
+    
     #rescale data so that mean of WT group is 1
     wt_og_mean = df %>%
         filter(strain=="WT", temperature==30) %>%
         distinct(group_mean) %>%
         pull(group_mean)
-
+    
     df %<>% mutate(spikenorm_scaled = scales::rescale(spikenorm, from=c(0, wt_og_mean)))
-
+    
     summary_df = df %>%
-        group_by(strain, temperature) %>%
+        group_by(strain, temperature) %>% 
         summarise(group_mean_scaled = mean(spikenorm_scaled),
                   group_sd = sd(spikenorm_scaled))
-
+    
     # barplot = ggplot() +
     #     geom_hline(yintercept = 0) +
     #     geom_col(data = summary_df, aes(x=interaction(strain, temperature),
@@ -67,7 +66,7 @@ main = function(theme_spec, data_path,
     text_edge = 0.3
     increment = (1-text_edge-0.04)/4
     start = increment/2+0.02
-
+    
     align1 = segmentsGrob(x0=text_edge+start,
                           x1=text_edge+start, y0=0, y1=1,
                           gp = gpar(lty="dashed"))
@@ -82,19 +81,19 @@ main = function(theme_spec, data_path,
                           gp = gpar(lty="dashed"))
     temp_line_one = segmentsGrob(x0=text_edge+start-0.06,
                                  x1=text_edge+start+increment+0.06,
-                                 y0=0.79, y1=0.79)
+                                 y0=0.785, y1=0.785)
     temp_line_two = segmentsGrob(x0=text_edge+start+2*increment-0.06,
                                  x1=text_edge+start+3*increment+0.06,
-                                 y0=0.79, y1=0.79)
+                                 y0=0.785, y1=0.785)
     temp_label_one = textGrob(label = expression(30*degree*C),
                               gp = gpar(fontsize = 7),
                               x=text_edge+start+increment/2,
-                              y=0.8,
+                              y=0.795,
                               vjust=0)
     temp_label_two = textGrob(label = expression(37*degree*C),
                               gp = gpar(fontsize = 7),
                               x=text_edge+start+5/2*increment,
-                              y=0.8,
+                              y=0.795,
                               vjust=0)
     spt6_label = textGrob(label = "Spt6-FLAG",
                           x = text_edge, y=0.66, gp = gpar(fontsize = 7),
@@ -120,21 +119,16 @@ main = function(theme_spec, data_path,
                           x = text_edge+start+3*increment, y=0.76,
                           gp = gpar(fontsize = 7,
                                     fontface = "italic"))
-    spt6_blot = rasterGrob(readPNG(spt6_blot_path),
-                      width=1-text_edge-0.04,
+    blot = rasterGrob(readPNG(blot_path),
+                      width=1-text_edge+.02,
                       x=text_edge + (1-text_edge)/2,
-                      height=0.15,
-                      y=unit(0.66, "npc"))
+                      height=0.37,
+                      y=unit(0.58, "npc"))
     spt6_outline = rectGrob(width=1-text_edge-0.04,
                             x=text_edge + (1-text_edge)/2,
                             height=0.15,
                             y=unit(0.66, "npc"),
                             gp=gpar(lwd=1,fill=NA))
-    dst_blot = rasterGrob(readPNG(dst1_blot_path),
-                      width=1-text_edge-0.04,
-                      x=text_edge + (1-text_edge)/2,
-                      height=0.15,
-                      y=unit(0.495, "npc"))
     dst_outline = rectGrob(width=1-text_edge-0.04,
                             x=text_edge + (1-text_edge)/2,
                             height=0.15,
@@ -156,20 +150,20 @@ main = function(theme_spec, data_path,
                                gp = gpar(fontsize=8),
                                x = text_edge+start+3*increment,
                                y = 0.365)
-
+    
     western = gTree(children = gList(
+        blot,
         # align1, align2, align3, align4,
         spt6_status, spt6_label, dst1_label,
         plus_one, plus_two,
         mutant_one, mutant_two,
         temp_line_one, temp_line_two,
         temp_label_one, temp_label_two,
-        spt6_blot, dst_blot,
         spt6_outline, dst_outline,
         quant_label_one, quant_label_two, quant_label_three, quant_label_four))
     fig_one_b = western %>%
         add_label("B")
-
+    
     ggsave(svg_out, plot=fig_one_b, width=fig_width, height=fig_height, units="cm")
     ggsave(pdf_out, plot=fig_one_b, width=fig_width, height=fig_height, units="cm")
     ggsave(png_out, plot=fig_one_b, width=fig_width, height=fig_height, units="cm", dpi=326)
@@ -178,8 +172,7 @@ main = function(theme_spec, data_path,
 
 main(theme_spec = snakemake@input[["theme"]],
      data_path = snakemake@input[["data_path"]],
-     spt6_blot_path = snakemake@input[["spt6_blot_path"]],
-     dst1_blot_path = snakemake@input[["dst1_blot_path"]],
+     blot_path = snakemake@input[["blot_path"]],
      fig_width = snakemake@params[["width"]],
      fig_height = snakemake@params[["height"]],
      svg_out = snakemake@output[["svg"]],
